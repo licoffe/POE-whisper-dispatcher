7bb44b0fd3650f0e00beda273d1eece2224093da
Licoffe <p1lgr11m@gmail.com>
2016-03-22 17:30:50 +0100

    Initial commit
    Initial commit

---
+++ POE-whisper-dispatcher.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-03-19T18:36:08
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = POE-whisper-dispatcher
+TEMPLATE = app
+
+CONFIG += c++11 static
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.h
@@ -0,0 +1,52 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QProcess>
+#include <QFileDialog>
+#include <QLabel>
+#include <QDebug>
+#include <regex>
+#include <fstream>
+#include <thread>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+#include <QTimer>
+#include <QDir>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+		Q_OBJECT
+
+	public:
+		explicit MainWindow(QWidget *parent = 0);
+		~MainWindow();
+
+	private:
+		Ui::MainWindow *ui;
+		QFileDialog* file_dialog;
+		QLabel* statusMessage;
+		QString client_path;
+		QString poe_path;
+		std::thread parser_thread;
+		bool stop_parser_thread;
+		int last_line_count;
+		QTimer* interval;
+		void parse_file(const std::string file_path , const int start_line);
+		int count_lines( const std::string file_path );
+//		std::string exec( const char* cmd );
+
+	public slots:
+		void start();
+		void stop();
+		QString browse_to_client_file();
+		void quit();
+		void parse();
+};
+
+#endif // MAINWINDOW_H
